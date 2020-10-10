8f7f0fea46eda95d58bb9fcfb06d6dbd5fe2e706
Tatsuya.Machii <machii.tty@gmail.com>
2020-10-10 16:43:50 +0900

    ルートの順番変更

--- routes/web.php
+++ routes/web.php
@@ -28,10 +28,10 @@ Route::group(['middleware' => 'auth'], function() {
   // FolderPolicyのviewメソッドにより、自信と紐づかないFolderにアクセスした場合は403エラーを出力する。
   Route::group(['middleware' => 'can:view,folder'], function(){
     Route::get('folders/{folder}/tasks', [TaskController::class, 'index'])->name('tasks.index');
-    Route::get('folders/{folder}/tasks/{task}/edit', [TaskController::class, 'showEditForm'])->name('tasks.edit');
-    Route::post('folders/{folder}/tasks/{task}/edit', [TaskController::class, 'edit']);
     Route::get('folders/{folder}/tasks/create', [TaskController::class, 'showCreateForm'])->name('tasks.create');
     Route::post('folders/{folder}/tasks/create', [TaskController::class, 'create']);
+    Route::get('folders/{folder}/tasks/{task}/edit', [TaskController::class, 'showEditForm'])->name('tasks.edit');
+    Route::post('folders/{folder}/tasks/{task}/edit', [TaskController::class, 'edit']);
   });
 
 
